b51b896d6db252bc0e532e283d9cdca7e24be447
Your Name <you@example.com>
2019-10-25 22:47:55 -0400

    init

--- rubyvm/cmd/ruby/main.go
+++ rubyvm/cmd/ruby/main.go
@@ -17,8 +17,18 @@ func init() {
 	flag.BoolVar(verboseFlag, "v", false, "enables verbose mode")
 }
 
+// TODO: The goal should be to match the exact behavior the c version provides
+// so if no argument is provided it should not error, it should hold open and
+// and let ruby to be typed directly. Additionally we should provide all the
+// flags like -e for example
 func main() {
 	flag.Parse()
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("it should hold open and allow user to type in ruby, but currently requires a filename argument, try again with a filename")
+		os.Exit(1)
+	}
 
 	// for now, assumes this is only being invoked with a filename to interpret
 	file, err := os.Open(flag.Args()[0])
@@ -32,9 +42,9 @@ func main() {
 	}
 
 	home := os.Getenv("HOME")
-	grubbyHome := filepath.Join(home, ".grubby")
+	rubyvmHome := filepath.Join(home, ".rubyvm")
 
-	rubyVM := vm.NewVM(grubbyHome, flag.Args()[0])
+	rubyVM := vm.NewVM(rubyvmHome, flag.Args()[0])
 	defer rubyVM.Exit()
 
 	_, err = rubyVM.Run(string(bytes))
